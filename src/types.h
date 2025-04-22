86cb2ec73dd5b9214def753ed73bd83979f2b1af
Caleb 'Pixenal' Dawson <pixenal.contact@gmail.com>
2025-04-22 19:07:38 +1000

    Add types header for internal use

---
+++ src/types.h
@@ -0,0 +1,54 @@
+/* 
+SPDX-FileCopyrightText: 2025 Caleb Dawson
+SPDX-License-Identifier: Apache-2.0
+*/
+
+#pragma once
+
+#include <uv_stucco.h>
+
+typedef int8_t I8;
+typedef int16_t I16;
+typedef int32_t I32;
+typedef int64_t I64;
+
+typedef uint8_t U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64;
+
+typedef float F32;
+typedef double F64;
+
+typedef uint8_t UBitField8;
+typedef uint16_t UBitField16;
+typedef uint32_t UBitField32;
+typedef uint64_t UBitField64;
+
+typedef PixtyRange Range;
+
+typedef PixtyI32Arr I32Arr;
+typedef PixtyI8Arr I8Arr;
+
+typedef PixtyV2_I8 V2_I8;
+typedef PixtyV2_I16 V2_I16;
+typedef PixtyV2_I32 V2_I32;
+typedef PixtyV2_I64 V2_I64;
+typedef PixtyV2_F32 V2_F32;
+typedef PixtyV2_F64 V2_F64;
+typedef PixtyV3_I8 V3_I8;
+typedef PixtyV3_I16 V3_I16;
+typedef PixtyV3_I32 V3_I32;
+typedef PixtyV3_I64 V3_I64;
+typedef PixtyV3_F32 V3_F32;
+typedef PixtyV3_F64 V3_F64;
+typedef PixtyV4_I8 V4_I8;
+typedef PixtyV4_I16 V4_I16;
+typedef PixtyV4_I32 V4_I32;
+typedef PixtyV4_I64 V4_I64;
+typedef PixtyV4_F32 V4_F32;
+typedef PixtyV4_F64 V4_F64;
+typedef PixtyM4x4 M4x4;
+typedef PixtyM3x3 M3x3;
+typedef PixtyM2x2 M2x2;
+typedef PixtyM2x3 M2x3;
